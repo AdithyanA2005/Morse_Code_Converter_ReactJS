86159ab9758f488b2239c60c996e77ed53fc3fca
Adithyan A <69440263+AdithyanA2005@users.noreply.github.com>
2022-09-07 12:04:28 +0000

    Morse Values made into a object

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 function Header() {
-  const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 1.5rem;
-  `;
-
-  const Heading = styled.h1`
-    color: #a8dadc;
-    font-size: 2.5rem;
-  `;
-
   return (
     <>
       <Container>
@@ -22,5 +10,16 @@ function Header() {
     </>
   );
 }
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  padding: 1.5rem;
+`;
+const Heading = styled.h1`
+  color: #a8dadc;
+  font-size: 2.5rem;
+`;
 
 export default Header;
